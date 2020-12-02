11bceb6543625309306b3b56eb8e063cce9c0892
Jaime <solucionesinformaticasjtc@gmail.com>
2020-12-01 23:54:07 -0300

    initial project

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app >
+
+    <v-app-bar
+      fixed
+      app
+      color="blue"
+    >
+
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer>
+<!-- aqui va el footer -->
+  </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ pages/contactanos.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Nueva pag</h1>  
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
